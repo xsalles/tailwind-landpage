ab0cc51e4842ff7ba8a37cedc9367c163ea4d774
xsalles <xs.salles@gmail.com>
2025-04-13 09:47:48 -0300

    feat: created constants of all illustration to avoid repeat code

---
+++ src/constants/illustrations.ts
@@ -0,0 +1,49 @@
+import contact from  "../assets/illustrations/contact/illustration-contact.webp"
+
+import magnifier from  "../assets/illustrations/services/illustration_magnifier.webp"
+import analytics from  "../assets/illustrations/services/illustration-analytics.webp"
+import click from  "../assets/illustrations/services/illustration-click.webp"
+import content from  "../assets/illustrations/services/illustration-content.webp"
+import email from  "../assets/illustrations/services/illustration-email.webp"
+import emoji from  "../assets/illustrations/services/illustration-emoji.webp"
+
+import megaphone from "../assets/illustrations/hero/illustration_megaphone.webp"
+
+import sarah from "../assets/illustrations/people/person-sarah.webp"
+import john from "../assets/illustrations/people/picture-john.webp"
+import brian from "../assets/illustrations/people/picture-brian.webp"
+import emily from "../assets/illustrations/people/picture-emily.webp"
+import jane from "../assets/illustrations/people/picture-jane.webp"
+import michael from "../assets/illustrations/people/picture-michael.webp"
+
+import proposal from "../assets/illustrations/proposal/illustration-proposal.webp"
+
+export const illustrationsContact = {
+    contact
+}
+
+export const illustrationsServices = {
+    analytics,
+    click,
+    content,
+    email,
+    magnifier,
+    emoji,
+}
+
+export const illustrationsHero = {
+    megaphone
+}
+
+export const illustrationsPeople = {
+    sarah,
+    john,
+    brian,
+    emily,
+    jane,
+    michael
+}
+
+export const illustrationsProposal = {
+    proposal
+}
