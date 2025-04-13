3bc22f3bcc2828acbb599038279b026ef1d9762b
xsalles <xs.salles@gmail.com>
2025-04-13 09:55:01 -0300

    feat: created constants of all icons to avoid repeat cod.e

---
+++ src/constants/icons.ts
@@ -0,0 +1,67 @@
+import green from "../assets/icons/arrows/Icon-arrow-green.svg"
+import black from "../assets/icons/arrows/Icon-arrow-black.svg"
+import white from "../assets/icons/arrows/Icon-arrow-white.svg"
+
+import amazon from "../assets/icons/company/amazon.svg"
+import dribble from "../assets/icons/company/dribble.svg"
+import hubspot from "../assets/icons/company/hubspot.svg"
+import netflix from "../assets/icons/company/netflix.svg"
+import notion from "../assets/icons/company/notion.svg"
+import zoom from "../assets/icons/company/zoom.svg"
+
+import plus from "../assets/icons/dropdown/plus.svg"
+import minus from "../assets/icons/dropdown/minus.svg"
+
+import facebook from "../assets/icons/footer/facebook.svg"
+import linkedinWhite from "../assets/icons/footer/linkedin-white.svg"
+import twitter from "../assets/icons/footer/twitter.svg"
+
+import blackGreen from "../assets/icons/links/Icon-black-green.svg"
+import blackWhite from "../assets/icons/links/Icon-black-white.svg"
+import greenBlack from "../assets/icons/links/Icon-green-black.svg"
+import greenWhite from "../assets/icons/links/Icon-green-white.svg"
+import whiteBlack from "../assets/icons/links/Icon-white-black.svg"
+import whiteGreen from "../assets/icons/links/Icon-white-green.svg"
+
+import logoWhite from "../assets/icons/logos/logoWhite.svg"
+import logoBlack from "../assets/icons/logos/Logo-black.svg"
+
+export const logos = {
+    logoWhite,
+    logoBlack
+}
+
+export const arrows = {
+    green,
+    black,
+    white
+}
+
+export const iconsCompany = {
+    amazon,
+    dribble,
+    hubspot,
+    netflix,
+    notion,
+    zoom
+}
+
+export const iconsDropdown = {
+    plus,
+    minus
+}
+
+export const iconsFooter = {
+    facebook,
+    linkedinWhite,
+    twitter
+}
+
+export const iconsLinks = {
+    blackGreen,
+    blackWhite,
+    greenBlack,
+    greenWhite,
+    whiteBlack,
+    whiteGreen
+}
